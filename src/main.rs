3796b99e2f23a8fbdc1945650f4490b198d1f03d
A Duck <duckquack123321@protonmail.com>
2022-09-06 20:17:12 +0000

    garble

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    wind.end();
+    wind.show();
+    app.run().unwrap();
 }
